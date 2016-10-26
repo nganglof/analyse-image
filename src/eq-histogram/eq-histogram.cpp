cc01377ea08792b08ef98a1750573d79cbdd0cf7
Nolwenn <nolwenn.gangloff@gmail.com>
2016-10-26 15:29:35 +0200

    continuation hist (check diff ?)

--- src/eq-histogram/eq-histogram.cpp
+++ src/eq-histogram/eq-histogram.cpp
@@ -6,74 +6,92 @@
 using namespace cv;
 using namespace std;
 
+float
+leveledValue(float value,float min,float max){
+	float ret = value;
+	if(value <= min)
+		ret = min;
+	else if (value>=max)
+		ret =max;
+	return ret;
+}
+
 void
 process(const char* imsname)
 {
-	Mat imsimg = imread(imsname,1);
+	Mat imsimg = imread(imsname,0);
 
 	if(!imsimg.data){
 		std::cerr << "No image data" << endl;
 		exit(EXIT_FAILURE);
 	}
 	
-	unsigned int histogram[256];
-	float histogramNorm[256];
+	float histogram[256];
+	float histogramCumNorm[256];
 	Size imssize = imsimg.size();
 	unsigned int width = imssize.width;
 	unsigned int height = imssize.height;
-	
-	Mat imdimg(height, width, CV_8UC3);
+	unsigned int intmax = 0;
+
+	Mat imdimg(height, width, CV_8UC1);
+	Mat imdimgcv(height, width, CV_8UC1);
+	Mat imdimgdiff(height, width, CV_8UC1);
+	equalizeHist(imsimg,imdimgcv);
 	
 	for(unsigned int i = 0; i < 256; ++i)
 	{
 		histogram[i] = 0;
+		histogramCumNorm[i] = 0;
 	} 
 	
-	
-	/* creation histogramme */
+	//creation histogramme
 	for(unsigned int y = 0 ; y < (height) ; y++)
 	{
 		for(unsigned int x = 0 ; x < (width) ; x++)
 		{
-			std::cout << imsimg.at<Vec3b>(y,x)[0] << endl;
-			histogram[imsimg.at<Vec3b>(y,x)[0]]++;
+			int value = imsimg.at<uchar>(y,x);
+			histogram[value]++;
 		}
 	}
 	
-	unsigned int intmax = 0;
-	/* intmax */
+	//intmax
 	for(unsigned int i = 0; i < 256; ++i)
 	{
-		if(intmax < histogram[i])
-		{
-			intmax = histogram[i];
-		}
+		if((histogram[i]!= 0) && (i>intmax))
+			intmax = i;
 	} 
 	
+	//histogramme normalisé
+	float NM = (float)height * (float)width;
+	for(unsigned int i = 0; i < 256; ++i)
+	{
+		histogram[i] = leveledValue(((float)histogram[i])/NM,0,1);
+	} 
+
+	//histogramme cummulé normalisé
 	for(unsigned int i = 0; i < 256; ++i)
 	{
 		for(unsigned int j = 0; j <= i; ++j)
 		{
-			histogramNorm[i] += histogram[j];
+			histogramCumNorm[i] += histogram[j];
 		}
+		histogramCumNorm[i] = leveledValue(histogramCumNorm[i],0,1);
 	}
-	
-	for(unsigned int i = 0; i < 256; ++i)
-	{
-		histogramNorm[i] = (float)histogramNorm[i] / ((float)height * (float)width);
-	} 
+
 	
 	for(unsigned int y = 0 ; y < (height) ; y++)
 	{
 		for(unsigned int x = 0 ; x < (width) ; x++)
 		{
-			imdimg.at<Vec3b>(y,x)[0]= (int)(intmax * histogramNorm[imsimg.at<Vec3b>(y,x)[0]]) ;
-			imdimg.at<Vec3b>(y,x)[1]= (int)(intmax * histogramNorm[imsimg.at<Vec3b>(y,x)[0]]) ;
-			imdimg.at<Vec3b>(y,x)[2]= (int)(intmax * histogramNorm[imsimg.at<Vec3b>(y,x)[0]]) ;
+			int value = imsimg.at<uchar>(y,x);
+			imdimg.at<uchar>(y,x)= (int)leveledValue((((float)intmax) * histogramCumNorm[value]),0.,255.) ;
+			imdimgdiff.at<uchar>(y,x) = imdimgcv.at<uchar>(y,x)- imdimg.at<uchar>(y,x);
 		}
 	}
 	
 	imwrite("eq.png",imdimg);
+	imwrite("eq-cv.png",imdimgcv);
+	imwrite("diff.png",imdimgdiff);
 }
 
 void
