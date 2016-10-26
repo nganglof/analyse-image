35eb3bcb9396139a4506e35f38f227b146cb237a
Nolwenn <nolwenn.gangloff@gmail.com>
2016-10-26 10:27:43 +0200

    readwriteshow.cpp

--- src/readwriteshow/readwriteshow.cpp
+++ src/readwriteshow/readwriteshow.cpp
@@ -9,7 +9,30 @@ using namespace std;
 void
 process(const char* imsname, const char* imdname)
 {
+	Mat imsimg = imread(imsname,1);
 
+	if(!imsimg.data){
+		std::cout << "No image data" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	Mat imdimg(100,200,CV_8UC3, Scalar(255,0,255));
+
+	namedWindow("Display window imd",WINDOW_AUTOSIZE);
+	imshow("Display window imd",imdimg);
+
+	namedWindow("Display window ims",WINDOW_AUTOSIZE);
+	imshow("Display window ims",imsimg);
+
+	Size imssize = imsimg.size();
+	int width = imssize.width;
+	int height = imssize.height;
+
+	std::cout << "Dimension " << imsname << " : " << width << "x" << height << endl;
+
+	imwrite(imdname,imdimg);
+
+	waitKey(0);
 }
 
 void
