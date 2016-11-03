13a0dfb07c0fcd4cf4c6b08c88b398545edd1034
Nolwenn <nolwenn.gangloff@gmail.com>
2016-11-03 12:09:57 +0100

    suite nl-means

--- td2/nl-means/nl-means.cpp
+++ td2/nl-means/nl-means.cpp
@@ -7,8 +7,35 @@
 using namespace cv;
 using namespace std;
 
+bool
+isCorrect(int i, int j, Mat mat){
+	return (i >= 0 && j >= 0 && i < mat.size().height && j < mat.size().width);
+}
+
+float
+patchesDistance(Mat mat, int p[], int q[], int n){
+	float S = 0;
+	int pxi,pxj,qxi,qxj;
+	for(int i = -n; i < n; ++i)
+	{
+		for(int j = -n; j < n; ++j)
+		{
+			pxi = p[0] + i;
+			pxj = p[1] + j;
+			qxi = q[0] + i;
+			qxj = q[1] + j;
+			if(isCorrect(pxi, pxj, mat) && isCorrect(qxi, qxj, mat))
+			{
+				S += pow(mat.at<uchar>(pxi, pxj) - mat.at<uchar>(qxi, qxj), 2); 
+			}
+			
+		}
+	}
+	return S / pow(2*n+1,2);
+}
+
 void
-process(int radius, const char* imsname, const char* imdname)
+process(float sigma, const char* imsname, const char* imdname)
 {
 	Mat imsimg = imread(imsname,0);
 
@@ -23,6 +50,28 @@ process(int radius, const char* imsname, const char* imdname)
 
 	Mat imdimg(height, width, CV_8UC1);
 
+
+	for(unsigned int i = 0 ; i < height ; ++i)
+	{
+		for(unsigned int j = 0 ; j < width ; ++j)
+		{
+			unsigned int iMin = std::max(0, (int) i - radius);
+			unsigned int iMax = std::min((int) height-1, (int) i + radius);
+			unsigned int jMin = std::max(0, (int) j - radius);
+			unsigned int jMax = std::min((int) width-1, (int) j + radius);
+					
+			float w;
+
+			for(unsigned int x = jMin; x <= jMax; ++x){
+				for(unsigned int y = iMin; y <= iMax; ++y){
+					
+				}
+			}
+
+
+
+		}
+	}
 
 
 	imwrite(imdname,imdimg);
